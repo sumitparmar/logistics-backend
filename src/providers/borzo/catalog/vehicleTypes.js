87c1b7eb38c1ec33bdd59911a905d30471d6f36d
Sumit Parmar <sumitparmar496@gmail.com>
2026-04-28 13:10:26 +0530

    backend changes added

--- src/providers/borzo/catalog/vehicleTypes.js
+++ src/providers/borzo/catalog/vehicleTypes.js
@@ -1,7 +1,1 @@
-module.exports = [
-  { id: 1, name: "Mini 3-Wheeler", maxWeightKg: 100 },
-  { id: 2, name: "Tata Ace 8ft", maxWeightKg: 1000 },
-  { id: 3, name: "Tata Ace 7ft", maxWeightKg: 750 },
-  { id: 5, name: "Tempo Truck", maxWeightKg: 200 },
-  { id: 8, name: "Motorbike", maxWeightKg: 20 },
-];
+module.exports = require("../../../constants/vehicleTypes");
